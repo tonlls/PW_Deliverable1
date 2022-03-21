c24003115c603ab1dcfc197b4c8a99c2d0e31e6f
tonlls <tonlls1999@gmail.com>
2022-03-21 21:52:29 +0100

    secret key fix

--- Deliverable1/settings.py
+++ Deliverable1/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'django-insecure-q#ghs0_!n12^#bm5_7*zl(7qh!5o5q!6#vu*=rcgjk5x@44mn_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
